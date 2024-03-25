90db2285f0161b44794c62a70c05f75685a700bc
Ziyu Huang <ziyu4huang@gmail.com>
2024-03-26 03:32:51 +0800

    for readbility

--- sd/pipeline.py
+++ sd/pipeline.py
@@ -3,6 +3,10 @@ import numpy as np
 from tqdm import tqdm
 from ddpm import DDPMSampler
 from transformers import CLIPTokenizer
+from clip import CLIP
+from encoder import VAE_Encoder
+from decoder import VAE_Decoder
+from diffusion import Diffusion
 
 WIDTH = 512
 HEIGHT = 512
@@ -40,9 +44,10 @@ def generate(
         else:
             generator.manual_seed(seed)
 
-        clip = models["clip"]
-        clip.to(device)
+        clip_obj: CLIP = models["clip"]
+        clip_obj.to(device)
         
+        # do Classfier Free Guidance
         if do_cfg:
             # Convert into a list of length Seq_Len=77
             cond_tokens = tokenizer.batch_encode_plus(
@@ -51,7 +56,7 @@ def generate(
             # (Batch_Size, Seq_Len)
             cond_tokens = torch.tensor(cond_tokens, dtype=torch.long, device=device)
             # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
-            cond_context = clip(cond_tokens)
+            cond_context = clip_obj(cond_tokens)
             # Convert into a list of length Seq_Len=77
             uncond_tokens = tokenizer.batch_encode_plus(
                 [uncond_prompt], padding="max_length", max_length=77
@@ -59,7 +64,7 @@ def generate(
             # (Batch_Size, Seq_Len)
             uncond_tokens = torch.tensor(uncond_tokens, dtype=torch.long, device=device)
             # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
-            uncond_context = clip(uncond_tokens)
+            uncond_context = clip_obj(uncond_tokens)
             # (Batch_Size, Seq_Len, Dim) + (Batch_Size, Seq_Len, Dim) -> (2 * Batch_Size, Seq_Len, Dim)
             context = torch.cat([cond_context, uncond_context])
         else:
@@ -70,8 +75,8 @@ def generate(
             # (Batch_Size, Seq_Len)
             tokens = torch.tensor(tokens, dtype=torch.long, device=device)
             # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
-            context = clip(tokens)
-        to_idle(clip)
+            context = clip_obj(tokens)
+        to_idle(clip_obj)
 
         if sampler_name == "ddpm":
             sampler = DDPMSampler(generator)
@@ -82,8 +87,8 @@ def generate(
         latents_shape = (1, 4, LATENTS_HEIGHT, LATENTS_WIDTH)
 
         if input_image:
-            encoder = models["encoder"]
-            encoder.to(device)
+            encoder_obj: VAE_Encoder = models["encoder"]
+            encoder_obj.to(device)
 
             input_image_tensor = input_image.resize((WIDTH, HEIGHT))
             # (Height, Width, Channel)
@@ -100,20 +105,20 @@ def generate(
             # (Batch_Size, 4, Latents_Height, Latents_Width)
             encoder_noise = torch.randn(latents_shape, generator=generator, device=device)
             # (Batch_Size, 4, Latents_Height, Latents_Width)
-            latents = encoder(input_image_tensor, encoder_noise)
+            latents = encoder_obj(input_image_tensor, encoder_noise)
 
             # Add noise to the latents (the encoded input image)
             # (Batch_Size, 4, Latents_Height, Latents_Width)
             sampler.set_strength(strength=strength)
             latents = sampler.add_noise(latents, sampler.timesteps[0])
 
-            to_idle(encoder)
+            to_idle(encoder_obj)
         else:
             # (Batch_Size, 4, Latents_Height, Latents_Width)
             latents = torch.randn(latents_shape, generator=generator, device=device)
 
-        diffusion = models["diffusion"]
-        diffusion.to(device)
+        diffusion_obj: Diffusion = models["diffusion"]
+        diffusion_obj.to(device)
 
         timesteps = tqdm(sampler.timesteps)
         for i, timestep in enumerate(timesteps):
@@ -129,7 +134,7 @@ def generate(
 
             # model_output is the predicted noise
             # (Batch_Size, 4, Latents_Height, Latents_Width) -> (Batch_Size, 4, Latents_Height, Latents_Width)
-            model_output = diffusion(model_input, context, time_embedding)
+            model_output = diffusion_obj(model_input, context, time_embedding)
 
             if do_cfg:
                 output_cond, output_uncond = model_output.chunk(2)
@@ -138,13 +143,13 @@ def generate(
             # (Batch_Size, 4, Latents_Height, Latents_Width) -> (Batch_Size, 4, Latents_Height, Latents_Width)
             latents = sampler.step(timestep, latents, model_output)
 
-        to_idle(diffusion)
+        to_idle(diffusion_obj)
 
-        decoder = models["decoder"]
-        decoder.to(device)
+        decoder_obj: VAE_Decoder = models["decoder"]
+        decoder_obj.to(device)
         # (Batch_Size, 4, Latents_Height, Latents_Width) -> (Batch_Size, 3, Height, Width)
-        images = decoder(latents)
-        to_idle(decoder)
+        images = decoder_obj(latents)
+        to_idle(decoder_obj)
 
         images = rescale(images, (-1, 1), (0, 255), clamp=True)
         # (Batch_Size, Channel, Height, Width) -> (Batch_Size, Height, Width, Channel)
